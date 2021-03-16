5bb05c656e338b6f5f5945cdb3b3c61d5792a200
makscee <shadeoflance@gmail.com>
2021-03-16 16:04:06 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  Container,
+  Row,
+  Col,
+} from 'reactstrap';
+import GameCard from './GameCard';
+import GreetingsHeader from './GreetingsHeader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import duamoCard from './assets/duamo_card.png'
+import sfCard from './assets/squarefast_card.png'
+import { ContactBlock } from './ContactBlock';
+
+const openInNewTab = (url) => {
+  const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+  if (newWindow) newWindow.opener = null
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+      showDuamoEmbed: false,
+      showSfEmbed: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Container className="main-container">
+          <Row>
+            <Col>
+              <GreetingsHeader />
+            </Col>
+          </Row>
+          <Row>
+            <div className="white-div">
+              <div className="dark-div">
+                <GameCard
+                  img={duamoCard}
+                  text={`duamo is a minimalistic puzzle game\nwhere you have to fit shapes into a square`}
+                  title="duamo"
+                  onClickPlay={() => openInNewTab('https://maks-c.itch.io/prt2')}
+                />
+                <GameCard
+                  img={sfCard}
+                  text={`Square Fast is a fast-paced hardcore arcade\nwith rhythm elements`}
+                  title="Square Fast"
+                  onClickPlay={() => openInNewTab('https://store.steampowered.com/app/1269560/Square_Fast/')}
+                  onClickVideo={() => this.setState({ showSfEmbed: !this.state.showSfEmbed })}
+                  embedId="K-c2ZGTSARs"
+                  showEmbed={this.state.showSfEmbed}
+                  videoBtnEnabled={true}
+                />
+              </div>
+            </div>
+          </Row>
+          <Row>
+            <div className="white-div">
+              <div className="dark-div">
+                <ContactBlock
+                  onClick={() => openInNewTab('https://mailchi.mp/20fe1b8c9b54/maksim-chugunov-website')}
+                />
+              </div>
+            </div>
+
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
